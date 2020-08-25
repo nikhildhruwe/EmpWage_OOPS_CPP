87e16997cf9047ec7939df9d01890e2723584d03
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-26 00:57:15 +0530

    Added: Code to get daily wage for part time employee

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -5,32 +5,33 @@ using namespace std;
 class Employee{
     private:
         int EMP_RATE_PER_HOUR = 20;
-        int empHrs;
     public :
-        int checkAttendance();
+        int getEmployeeType();
         int getDailyWage(int);
 };
 
-int Employee :: checkAttendance(){
+int Employee :: getEmployeeType(){
     srand(time(0));
-    return (rand() % 2);  
+    return (rand() % 3);  
 }
 
-int Employee :: getDailyWage(int attendance){
-    if (attendance == 1)
+int Employee :: getDailyWage(int empType){
+    int FULL_TIME = 0, PART_TIME = 1, empHrs;;
+
+    if ( empType == FULL_TIME)
         empHrs = 8;
+    else if ( empType == PART_TIME)
+        empHrs = 4;
     else
         empHrs = 0;
-   return empHrs * EMP_RATE_PER_HOUR;
+    return empHrs * EMP_RATE_PER_HOUR;
 }
 
 int main(){
     cout << "\n\tWelcome To Employee Wage Computation" << endl;
-    int attendance, empWage;
+    int empType, empWage;
     Employee empObj;
-    attendance = empObj.checkAttendance();
-    empWage = empObj.getDailyWage(attendance);
+    empType = empObj.getEmployeeType();
+    empWage = empObj.getDailyWage(empType);
     cout << "Employee Wage Per Day = " << empWage << endl;
-
-
 }
