5bf51890c768cf5a545f67c08493d05957e124c0
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-25 22:50:47 +0530

    Added: Code to check employee attendance

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 class Employee{
+    public :
+        int getAttendance(){
+            srand(time(0));
+            return (rand() % 2);
+        }
+
 };
 
 int main(){
     cout << "\n\tWelcome To Employee Wage Computation" << endl;
+    Employee empObj;
+    int attendance = empObj.getAttendance();
+
+    if (attendance == 1) {
+        cout << "Employee is Present" << endl;
+    }
+    else {
+        cout << "Employee is Absent" << endl;
+    }    
+    return 0;
 }
